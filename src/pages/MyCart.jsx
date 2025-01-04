b8d0ed0ebe85ab9d6d91a6d179be21f50abf3183
timotius giovandi <tim.gio@outlook.com>
2025-01-04 16:03:24 +0100

    add empty cart and checkout button

--- src/pages/MyCart.jsx
+++ src/pages/MyCart.jsx
@@ -1,9 +1,17 @@
+
+import { BasicModal } from "../ui/Modals";
 import { Sidecart } from "../ui/Sidecart";
 
 export function MyCart(){
     return(
-        <div className="w-full">
+        <div className="flex flex-col bg-primary-200 w-full justify-between">
+
             <Sidecart/>
+
+
+            <div className="py-3">
+            <BasicModal/>
+            </div>
         </div>
     )
 }
--- src/ui/Sidecart.jsx
+++ src/ui/Sidecart.jsx
@@ -2,26 +2,34 @@ import { useSelector } from "react-redux";
 import { getCart } from "../store";
 import { CartItemCard } from "./CartItemCard";
 import { IoIosCart } from "react-icons/io";
+import { useNavigate } from "react-router";
 
 
 
 export function Sidecart() {
   const data = useSelector(getCart);
+  const navigate = useNavigate()
+
+  function shopNowHandler(){
+    return(
+      navigate('/')
+    )
+  }
   // console.log(data.length);
 
   function NoItem(){
     return (
-      <div className="w-full h-full flex flex-col justify-center items-center align-middle">
+      <div className="w-full flex flex-col justify-center items-center align-middle">
         <IoIosCart size={60} className="text-primary-500"/>
         <h1 className="text-gray-800">no item in cart</h1>
-        <button className="block md:hidden mt-3 bg-primary-400 w-1/2 py-1 rounded-3xl text-white h-8 md:h-10 hover:bg-primary-500 transition-colors">shop now</button>
+        <button className="block md:hidden mt-3 bg-primary-400 w-1/2 py-1 rounded-3xl text-white h-8 md:h-10 hover:bg-primary-500 transition-colors" onClick={shopNowHandler}>shop now</button>
       </div>
     );
   } 
 
 
   return (
-      <div className={'bg-primary-200 overflow-scroll h-full transition-opacity duration-500 ease-in-out no-scrollbar'}>
+      <div className={`${!data.length?"h-full flex justify-center items-center align-middle":"h-fit"} overflow-scroll transition-opacity duration-500 ease-in-out no-scrollbar`}>
         {data.map((each) => (
           <CartItemCard key={each.itemid} dataEach={each} />
         ))}
